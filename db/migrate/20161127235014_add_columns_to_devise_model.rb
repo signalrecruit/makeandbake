8be9e8f0285b392e63b7a1bd2106db804625d722
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-11-28 00:16:34 +0000

    add attributes to user model

---
+++ db/migrate/20161127235014_add_columns_to_devise_model.rb
@@ -0,0 +1,10 @@
+class AddColumnsToDeviseModel < ActiveRecord::Migration
+  def change
+  	add_column :users, :first_name, :string
+  	add_column :users, :last_name, :string
+  	add_column :users, :username, :string
+  	add_column :users, :age, :integer, default: 0
+  	add_column :users, :seller, :boolean, default: false
+  	add_column :users, :admin, :boolean, default: false
+  end
+end
