530cad1c8c370252f839c06a86ee75d0b7f4732b
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-11-29 23:22:05 +0000

    deleted attachment model

--- spec/factories/attachments.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :attachment do
-    file "MyString"
-    product 
-  end
-end
--- spec/models/attachment_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Attachment, type: :model do
-end
