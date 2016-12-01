cd3bd957d432cedfe900d3790f04bd03b56264fa
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-01 13:27:01 +0000

    modifies simple search engine and advance search engine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :gender, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :gender, :image, :seller])
     
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :gender, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :gender, :image, :seller])
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -4,11 +4,12 @@ class Search < ActiveRecord::Base
     
     products = Product.all
 
-    products = Product.where(["lower(name) LIKE ?", "%#{keywords.downcase}%"]) if keywords.present?
+    products = products.where(["lower(name) LIKE ?", "%#{keywords.downcase}%"]) if keywords.present?
     # products = Product.where(["name LIKE ?", name]) if name.present?
-    products = Product.joins(:tags).where(tags: { name: name })
-    products = Product.where(["price >= ?", min_price]) if min_price.present?
-    products = Product.where(["price <= ?", max_price]) if max_price.present?
+    products = products.where(["price >= ?", min_price]) if min_price.present?
+    products = products.where(["price <= ?", max_price]) if max_price.present?
+    # products_by_tags = Product.joins(:tags).where(tags: { name: name })
+
 
     products
   end
