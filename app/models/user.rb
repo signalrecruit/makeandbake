b38c5540655621185953cd6c3b95730d2862d083
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-04 14:02:44 +0000

    commit changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,9 +34,8 @@ class User < ActiveRecord::Base
       # Get the existing user by email if the provider gives us a verified email.
       # If no verified email was provided we assign a temporary email and ask the
       # user to verify it on the next step via UsersController.finish_signup
-      # email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      # email = auth.info.email if email_is_verified
-      email = auth.info.email
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      email = auth.info.email if email_is_verified
       user = User.where(:email => email).first if email
 
       # Create the user if it's a new registration
@@ -44,8 +43,7 @@ class User < ActiveRecord::Base
         user = User.new(
           fullname: auth.extra.raw_info.name,
           username: auth.info.nickname || auth.uid,
-          # email: email ? auth.info.email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
-          email: auth.info.email,
+          email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
         )
         # user.skip_confirmation!
