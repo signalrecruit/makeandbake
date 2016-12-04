324394b041fe049b55899fdf8c457fa6b558f646
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-04 11:44:01 +0000

    commit changes to registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,4 +7,17 @@ class RegistrationsController < Devise::RegistrationsController
       products_path
   	end
   end
+
+  def finish_signup(resource)
+    # authorize! :update, @user 
+    if request.patch? && params[:user] #&& params[:user][:email]
+      if @user.update(user_params)
+        @user.skip_reconfirmation!
+        sign_in(@user, :bypass => true)
+        redirect_to @user, notice: 'Your profile was successfully updated.'
+      else
+        @show_errors = true
+      end
+    end
+  end
 end
