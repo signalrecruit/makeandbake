72d2efecef4a8f66c6167003e5faabb9dce772ce
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-04 11:55:36 +0000

    commit changes to omniauth_controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -29,4 +29,20 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # new_user_registration_url
     end
   end 
+
+
+  private 
+
+  def finish_signup(resource)
+    # authorize! :update, @user 
+    if request.patch? && params[:user] #&& params[:user][:email]
+      if @user.update(user_params)
+        @user.skip_reconfirmation!
+        sign_in(@user, :bypass => true)
+        redirect_to @user, notice: 'Your profile was successfully updated.'
+      else
+        @show_errors = true
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -67,4 +67,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+    match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
 end
