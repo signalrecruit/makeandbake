1d927f1e6586969b21532711669153ae1db12ea4
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-04 02:50:16 +0000

    commit

--- app/controllers/ominauth_callbacks_controller.rb
+++ app/controllers/ominauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class OminauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
    def self.provides_callback_for(provider)
     class_eval %Q{
