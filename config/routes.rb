549ce12f598740097a189df996a9cb901f17f299
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-04 12:00:26 +0000

    commit changes to omniauth_controller

--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-    match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+    match '/users/:id/finish_signup' => 'omniauth_callbacks#finish_signup', via: [:get, :patch], :as => :finish_signup
 
 end
