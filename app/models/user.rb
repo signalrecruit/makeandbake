36e6aea9a3dc06b078f185834aec41178b301d2c
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-04 22:53:44 +0000

    commit changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
         user = User.new(
           # twitter_image: auth.extra.raw_info.profile_image_url,
           fullname: auth.extra.raw_info.name || auth.info.name || "fullname here",
-          username: auth.info.nickname || auth.extra.raw_info.username  || auth.uid,
+          username: auth.info.nickname || auth.extra.raw_info.username || auth.uid,
           first_name: auth.extra.raw_info.first_name || "first name here",
           last_name: auth.extra.raw_info.last_name || "last name here",
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
