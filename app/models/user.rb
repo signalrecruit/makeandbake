3f1f64b3e8eb3df4a0463e44f93ae6f640cd843c
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-06 10:13:44 +0000

    modifies omniauth-facebook arguments

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
       if user.nil?
         user = User.new(
           # twitter_image: auth.extra.raw_info.profile_image_url,
-          fullname: auth.extra.raw_info.name || auth.info.name || "fullname here",
+          fullname: auth.extra.raw_info.name || auth.info.name,
           username: auth.info.nickname || auth.extra.raw_info.username || auth.uid,
           first_name: auth.extra.raw_info.first_name || "first name here",
           last_name: auth.extra.raw_info.last_name || "last name here",
