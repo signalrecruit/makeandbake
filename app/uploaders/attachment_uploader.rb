5413272d3c791fdd5b08aad631bf9563f0bed7ef
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-08 10:09:49 +0000

    commit before checkout

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -36,7 +36,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [250, 150]
+    process :resize_to_fit => [150, 150]
   end
 
   version :large do
