b71cb5536a7d3edc05aa956b329a7a1034198552
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-12-08 10:51:26 +0000

    resized profile image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [24, 24]
   end
 
+  version :medium do 
+    process :resize_to_fit => [150, 100]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
