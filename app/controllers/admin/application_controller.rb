ed409d21abaccdabe799dc54bc2f8d59201387b7
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-01-15 22:42:48 +0000

    commit before merge

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,6 +4,17 @@ class Admin::ApplicationController < ApplicationController
   layout "admin"
 
   def index
+    @users = User.all.where(admin: false)
+    @buyers = User.all.where(admin: false, seller: false)
+    @sellers = User.all.where(admin: false, seller: true)
+    @products = Product.all.where(approved: true)
+    @approved_products = Product.where(approved: true)
+    @unapproved_products = Product.where(approved: false)
+    @orders = Order.all
+    @shops = Shop.all.where(approved: true)
+    @shop_owners = User.all.where(admin: false, seller: true).joins(:shops)
+    @shopless_owners = User.all.where(admin: false).includes(:shops).where(shops: {user_id: nil})
+
   end
 
   private
