5b7921474a8279b0327f77d453a701ec7751129a
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-01-22 00:18:34 +0000

    changed domain to gmail.com in production settings for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
-    :domain => "https://makeandbakestage.herokuapp.com",
+    :domain => "gmail.com",
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
     :authentication => "plain",
