4c74db3bd7aba9605c874a245e1354449bc45074
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-01-24 12:16:14 +0000

    add seo url to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ActiveRecord::Base
   :delivery_date, :sender_name, :sender_address, :sender_phonenumber, :sender_email, presence: true
   validates :min_price, :max_price, numericality: { greater_than_or_equal_to: 0, only_float: true }
 
+ def to_param
+    "#{self.id}-#{self.tags.first.name.parameterize}"
+  end
+
 end
