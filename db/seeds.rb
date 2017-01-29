ef5185877c36b1d7118661d1012af7a0e4b46b16
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-01-29 15:14:15 +0000

    replaced text box with radio buttons for cake size

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,22 @@ categories.each do |category|
   Tag.find_or_create_by(name: category.downcase)
 end
 
-User.create first_name: "Derek", last_name: "Owusu-Frimpong", username: "paayaw", email: "deepsky_5@live.com", password: "password", password_confirmation: "password",
+# User.create first_name: "Derek", last_name: "Owusu-Frimpong", username: "paayaw", email: "deepsky_5@live.com", password: "password", password_confirmation: "password",
+#  phonenumber: "0204704427", gender: "male", age: 28, admin: true, seller: true, admin_access_level: :super_admin
+
+admin_super_one = {
+	first_name: "Derek", last_name: "Owusu-Frimpong", username: "paayaw", email: "deepsky_5@live.com", password: "password", password_confirmation: "password",
  phonenumber: "0204704427", gender: "male", age: 28, admin: true, seller: true, admin_access_level: :super_admin
+}
 
-# User.create( first_name: "Uchiha", last_name: "Sasuke", username: "Avenger", email: "sasuke@konoha.com", password: "password", password_confirmation: "password",
-#  phonenumber: "0204704427", gender: "male", age: 28, admin: true, seller: true)
+admin_super_two = {
+	first_name: "jide", last_name: "Otoki", username: "username", email: "jideotoki@gmail.com", password: "password", password_confirmation: "password",
+ phonenumber: "0000000000", gender: "male", age: 0, admin: true, seller: true, admin_access_level: :super_admin
+}
 
+array_of_super_admins = [admin_super_one, admin_super_two]
+
+
+array_of_super_admins.each do |admin_attributes|
+  User.create admin_attributes
+end
