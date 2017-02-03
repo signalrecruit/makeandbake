0ae81bc3a9cb8ef7b3601af4d44875d19de5e1ef
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-02-03 12:14:23 +0000

    uncomment 500 error handling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -5,11 +5,11 @@ class ErrorsController < ApplicationController
     render(:status => 404)
   end
 
-  # def internal_server_error
-  #   User.all.where(admin: true).each do |admin|
-  # 	  ServerErrorNotifierJob.set(wait: 5.seconds).perform_later(admin)
-  # 	end
+  def internal_server_error
+    User.all.where(admin: true).each do |admin|
+  	  ServerErrorNotifierJob.set(wait: 5.seconds).perform_later(admin)
+  	end
 
-  #   render(:status => 500)
-  # end
+    render(:status => 500)
+  end
 end
