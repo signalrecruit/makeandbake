d516d91b77a0e51d127f6df694ab6501f1360e03
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:22:24 +0000

    Update admin_notifier_for_shop_creation_job.rb

--- app/jobs/admin_notifier_for_shop_creation_job.rb
+++ app/jobs/admin_notifier_for_shop_creation_job.rb
@@ -2,8 +2,6 @@ class AdminNotifierForShopCreationJob < ActiveJob::Base
   queue_as :default
 
   def perform(admin, shop)
-  	@admin = admin
-  	@shop = shop
-  	NotifyAdminOfShopCreation.admin_notification(@admin, @shop).deliver_later
+  	NotifyAdminOfShopCreation.admin_notification(admin, shop).deliver_later
   end
 end
