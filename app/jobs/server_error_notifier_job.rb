e34d6ce4a1e1cbb94a79e8383eb5c106ab72d0de
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:25:49 +0000

    Update server_error_notifier_job.rb

--- app/jobs/server_error_notifier_job.rb
+++ app/jobs/server_error_notifier_job.rb
@@ -2,7 +2,6 @@ class ServerErrorNotifierJob < ActiveJob::Base
   queue_as :default
 
   def perform(admin)
-    @admin = admin
-    ServerError.server_error_notifier(@admin).deliver_later  
+    ServerError.server_error_notifier(admin).deliver_later  
   end
 end
