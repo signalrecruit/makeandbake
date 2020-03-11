e1b311391e4e1a6a74afcc71fd629c0cec2efb19
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:24:58 +0000

    Update seller_notifier_job.rb

--- app/jobs/seller_notifier_job.rb
+++ app/jobs/seller_notifier_job.rb
@@ -2,8 +2,6 @@ class SellerNotifierJob < ActiveJob::Base
   queue_as :default
 
   def perform(order, seller)
-    @order = order
-    @seller = seller
-    NotifySeller.seller_notification(@order, @seller).deliver_later
+    NotifySeller.seller_notification(order, seller).deliver_later
   end
 end
