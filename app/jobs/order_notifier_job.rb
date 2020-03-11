ee4b86c74f1c799c5c90340a568d0472208f7d63
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:23:27 +0000

    Update order_notifier_job.rb

--- app/jobs/order_notifier_job.rb
+++ app/jobs/order_notifier_job.rb
@@ -2,8 +2,6 @@ class OrderNotifierJob < ActiveJob::Base
   queue_as :default
 
   def perform(admin, order)
-  	@order = order
-  	@admin = admin
-    OrderNotification.notification_of_order(@admin, @order).deliver_later
+    OrderNotification.notification_of_order(admin, order).deliver_later
   end
 end
