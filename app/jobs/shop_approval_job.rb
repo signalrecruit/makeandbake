ba7a009f90d30cfae3d0576e8197d2dcf66ef693
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:26:19 +0000

    Update shop_approval_job.rb

--- app/jobs/shop_approval_job.rb
+++ app/jobs/shop_approval_job.rb
@@ -2,8 +2,6 @@ class ShopApprovalJob < ActiveJob::Base
   queue_as :default
 
   def perform(user, shop)
-    @user = user
-    @shop = shop
-    ApproveShop.shop_approval(@user, @shop).deliver_later  
+    ApproveShop.shop_approval(user, shop).deliver_later  
   end
 end
