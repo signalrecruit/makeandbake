25ad11156a39e416cbea8f33cf0833b0fa8863eb
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:21:57 +0000

    Update admin_signup_notifier_job.rb

--- app/jobs/admin_signup_notifier_job.rb
+++ app/jobs/admin_signup_notifier_job.rb
@@ -2,8 +2,6 @@ class AdminSignupNotifierJob < ActiveJob::Base
   queue_as :default
 
   def perform(admin, user)
-    @admin = admin
-    @user = user
-    NotifyAdminOfUser.notify_admin_of_signup(@admin, @user).deliver_later
+    NotifyAdminOfUser.notify_admin_of_signup(admin, user).deliver_later
   end
 end
